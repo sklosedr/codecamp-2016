1f17eca445f42faf47337c522ec2e90ae54fb2e3
eschrein <eschrein@nbnli00235.nli.local>
2016-10-21 13:47:16 +0200

    port config
    Change-Id: I83622b59a6c03dd87b7dfaea68ae71a2b0c07c13

--- codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
+++ codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.nextlevel.codecamp.user.controller;
 
-	import org.springframework.web.bind.annotation.RestController;
-	import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nextlevel.codecamp.model.user.DogUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.plaf.basic.BasicToolBarUI.DockingListener;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 
 	@RestController
 	public class UserController {
@@ -9,6 +17,13 @@ package com.nextlevel.codecamp.user.controller;
 	    @RequestMapping("/")
 	    public String index() {
 	        return "Greetings from Spring Boot!";
+	    }
+	    
+	    @RequestMapping("/users")
+	    public List<DogUser> getAllUsers() {
+	    	DogUser user1 = new DogUser();
+	    	user1.setUsername("Eugen");
+	    	return new ArrayList<DogUser>();
 	    }
 
 	}
