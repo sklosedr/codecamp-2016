fda55fd3381f739139e4429fc7315b589d2e1f8c
ngiriven <ngiriven@nbnli00756.nli.local>
2016-10-21 15:11:54 +0200

    addend User Change-Id: I170f153a820c6583d2ef6d255740f554b5c25efc

--- codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
+++ codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 import javax.swing.plaf.basic.BasicToolBarUI.DockingListener;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 	@RestController
 	public class UserController {
-//		private ArrayList<DogUser>
+		private List<DogUser> allUsers = new ArrayList<DogUser>();
 	    @RequestMapping("/")
 	    public String index() {
 	        return "Greetings from Spring Boot!";
@@ -21,9 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 	    
 	    @RequestMapping("/users")
 	    public List<DogUser> getAllUsers() {
-	    	DogUser user1 = new DogUser();
-	    	user1.setUsername("Eugen");
-	    	return new ArrayList<DogUser>();
+	    	
+	    	return allUsers;
 	    }
-
+	    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	    public DogUser post(@RequestBody DogUser dogUser){
+	    	allUsers.add(dogUser);
+	    	return dogUser;
+	    }
 	}
