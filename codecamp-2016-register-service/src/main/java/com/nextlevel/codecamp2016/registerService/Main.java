2fa383af3df8e93d74735ce3dd40fa9a2f85f2f8
bern <bern@nbnli00700.nli.local>
2016-10-21 10:42:54 +0200

    Added Spring Application Context

--- codecamp-2016-register-service/src/main/java/com/nextlevel/codecamp2016/registerService/Main.java
+++ codecamp-2016-register-service/src/main/java/com/nextlevel/codecamp2016/registerService/Main.java
@@ -1,9 +1,24 @@
 package com.nextlevel.codecamp2016.registerService;
 
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		ApplicationContext ctx = SpringApplication.run(Main.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
 	}
 
 }
