f07195ef63099d5c309fe0aa7e636a2b7bdc9a7b
a_eschrein <a_eschrein@nbnli00235.nli.local>
2016-10-21 16:48:31 +0200

    add database
    Change-Id: I9bbdbecff7588b253e940c674927e1b1ca9ee61f

--- codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
+++ codecamp-2016-user-service/src/main/java/com/nextlevel/codecamp/user/controller/UserController.java
@@ -3,18 +3,26 @@ package com.nextlevel.codecamp.user.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nextlevel.codecamp.model.user.DogUser;
+import com.nextlevel.codecamp.user.data.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.plaf.basic.BasicToolBarUI.DockingListener;
+import javax.transaction.Transactional;
 
+import org.apache.commons.collections4.IterableUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 	@RestController
 	public class UserController {
+		
+		@Autowired
+		private UserRepository userRepository;
+		
 		private List<DogUser> allUsers = new ArrayList<DogUser>();
 	    @RequestMapping("/")
 	    public String index() {
@@ -23,13 +31,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 	    
 	    @RequestMapping("/users")
 	    public List<DogUser> getAllUsers() {
-	    	
-	    	return allUsers;
+	    	return IterableUtils.toList(userRepository.findAll());
 	    }
+	    
 	    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	    @Transactional
 	    public DogUser post(@RequestBody DogUser dogUser){
-	    	
 	    	allUsers.add(dogUser);
+	    	userRepository.save(dogUser);
 	    	return dogUser;
 	    }
 	}
