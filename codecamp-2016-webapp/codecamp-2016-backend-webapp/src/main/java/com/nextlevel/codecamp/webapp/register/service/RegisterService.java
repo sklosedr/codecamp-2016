332bd5ec329d485588f1d9bfd0ac0e361b6b5c8f
agohr <agohr@nbnli00449.nli.local>
2016-10-22 17:06:15 +0200

    handling response in registerController web backend.

--- codecamp-2016-webapp/codecamp-2016-backend-webapp/src/main/java/com/nextlevel/codecamp/webapp/register/service/RegisterService.java
+++ codecamp-2016-webapp/codecamp-2016-backend-webapp/src/main/java/com/nextlevel/codecamp/webapp/register/service/RegisterService.java
@@ -1,6 +1,7 @@
 package com.nextlevel.codecamp.webapp.register.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.nextlevel.codecamp.model.register.Register;
@@ -12,8 +13,15 @@ public class RegisterService {
 	@Autowired
 	private RegisterClient registerClient;
 
-	public Register register(Register register) {
-		return registerClient.register(register);
+	public String register(Register register) {
+		try {
+			registerClient.register(register);
+			// TODO check return value
+			return HttpStatus.CREATED.toString();
+		} catch (RuntimeException e) {
+			// TODO log
+			return HttpStatus.INTERNAL_SERVER_ERROR.toString();
+		}
 	}
 
 }
