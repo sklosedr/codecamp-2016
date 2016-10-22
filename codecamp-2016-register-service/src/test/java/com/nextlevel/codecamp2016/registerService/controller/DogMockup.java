fc821426e1f96422bbe2377d88473030b32c0dbf
bern <bern@nbnli00700.nli.local>
2016-10-22 16:20:59 +0200

    Addapted tests

--- codecamp-2016-register-service/src/test/java/com/nextlevel/codecamp2016/registerService/controller/DogMockup.java
+++ codecamp-2016-register-service/src/test/java/com/nextlevel/codecamp2016/registerService/controller/DogMockup.java
@@ -1,13 +1,11 @@
 package com.nextlevel.codecamp2016.registerService.controller;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.nextlevel.codecamp.model.dog.Dog;
 import com.nextlevel.codecamp2016.registerService.client.DogClient;
 
 @Service
-@Primary
 public class DogMockup implements DogClient{
 
 	public Dog addDog(Dog dog) {
